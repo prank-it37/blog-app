fe81184f0b98746cf523aa5ad80d871ed23ae68e
Prantik <prantikd07@gmail.com>
2026-06-13 12:52:35 +0530

    first commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,39 @@
+# Application Factory
+# Dual purpose: 
+# creation of Flask instance inside a function
+# tells Python which flaskr directory should be treated as package
+
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True) # creates Flask instance
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from  . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
+    return app
